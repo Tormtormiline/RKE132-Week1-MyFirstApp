1298de9d5de84a45c482faa0ca8b78a56eec2818
Tormtormiline <toivo.luhaaar@tktk.ee>
2024-03-06 20:14:46 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more informatin
+
+string firstName, lastName;
+
+firstName = "Harry";
+lastName = "Potter";
+
+Console.WriteLine($"Hello,  {firstName}  lastName!");
+
